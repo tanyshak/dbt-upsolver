2098b5faebb755e798492616796c2bdc59db90dc
Tanya Shemet <tanyshak@gmail.com>
2023-01-06 14:47:50 +0100

    add upsolver slq as comments

--- examples/s3_to_athena/models/orders_transformed_data.sql
+++ examples/s3_to_athena/models/orders_transformed_data.sql
@@ -8,3 +8,28 @@ FROM {{ ref(‘orders_row_data’) }}
 LET customer_name = customer.firstname || ' ' || customer.lastname
 WHERE ordertype = 'SHIPPING'
 AND $event_time BETWEEN run_start_time() AND run_end_time();
+
+/*
+-- 5. Create an output table in AWS Glue Data Catalog. The table will be accessible in Athena once data has been added.
+
+CREATE TABLE default_glue_catalog.database_16e61b.orders_transformed_data(partition_date date)
+    PARTITIONED BY partition_date;
+
+-- 6. Create a streaming job to read from the staging table, apply business logic transformations and insert the results into the output table.
+CREATE SYNC JOB transform_orders_and_insert_into_athena
+    START_FROM = BEGINNING
+    ADD_MISSING_COLUMNS = true
+    RUN_INTERVAL = 1 MINUTE
+    AS INSERT INTO default_glue_catalog.database_16e61b.orders_transformed_data MAP_COLUMNS_BY_NAME
+    -- Use the SELECT statement to choose columns from the source and implement your business logic transformations.
+    SELECT
+      orderid AS order_id,
+      MD5(customer.email) AS customer_id, -- hash or mask columns using built-in functions
+      customer_name,  -- computed field defined later in the query
+      nettotal AS total,
+      $event_time AS partition_date
+    FROM default_glue_catalog.database_16e61b.orders_raw_data
+    LET customer_name = customer.firstname || ' ' || customer.lastname -- create a computed column
+    WHERE ordertype = 'SHIPPING'
+    AND $event_time BETWEEN run_start_time() AND run_end_time();
+*/
--- examples/s3_to_athena/models/s3_connection.sql
+++ examples/s3_to_athena/models/s3_connection.sql
@@ -1,1 +1,9 @@
 CREATE S3 CONNECTION {{ this }}
+
+/*
+-- 1. Create a connection to SQLake sample data source.
+CREATE S3 CONNECTION upsolver_s3_samples
+    AWS_ROLE = 'arn:aws:iam::949275490180:role/upsolver_samples_role'
+    EXTERNAL_ID = 'SAMPLES'
+    READ_ONLY = TRUE;
+*/
