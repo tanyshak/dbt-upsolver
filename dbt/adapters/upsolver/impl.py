b1478bd3e3e1c1e478b7f3545bf424b999652a93
Tanya Shemet <tanyshak@gmail.com>
2023-01-30 00:38:04 +0100

    create upsolver relation type class with new  materialization types

--- dbt/adapters/upsolver/impl.py
+++ dbt/adapters/upsolver/impl.py
@@ -5,21 +5,19 @@ from dbt.adapters.upsolver import UpsolverConnectionManager
 
 from dbt.events import AdapterLogger
 
-from dbt.adapters.base import BaseAdapter
-
-from dbt.events.functions import fire_event
-
 logger = AdapterLogger("Upsolver")
 
-from dbt.adapters.base.relation import BaseRelation
+from dbt.adapters.upsolver.relation import UpsolverRelation
 
 import agate
 
-from typing import Any, List
+from typing import Any, List, Optional
+
+
+
 
 LIST_RELATIONS_MACRO_NAME = "list_relations_without_caching"
 
-#from dbt.adapters.base.impl import BaseAdapter
 
 class UpsolverAdapter(adapter_cls):
     """
@@ -27,6 +25,7 @@ class UpsolverAdapter(adapter_cls):
     """
 
     ConnectionManager = UpsolverConnectionManager
+    Relation = UpsolverRelation
 
     @classmethod
     def date_function(cls):
@@ -38,12 +37,12 @@ class UpsolverAdapter(adapter_cls):
     def debug_query(self) -> None:
         self.execute('SELECT * FROM SystemTables.logs.task_executions limit 1;')
 
-    def create_schema(self, relation: BaseRelation) -> None:
+    def create_schema(self, relation: UpsolverRelation) -> None:
         logger.debug(f"+++++++++++++++++++++++++++++++++")
         logger.debug(f"Pass create schema")
         pass
 
-    def drop_schema(self, relation: BaseRelation) -> None:
+    def drop_schema(self, relation: UpsolverRelation) -> None:
         logger.debug(f"+++++++++++++++++++++++++++++++++")
         logger.debug(f"Pass drop schema")
         pass
@@ -51,27 +50,12 @@ class UpsolverAdapter(adapter_cls):
 
     def list_relations_without_caching(
         self,
-        schema_relation: BaseRelation,
-        ) -> List[BaseRelation]:
+        schema_relation: UpsolverRelation,
+        ) -> List[UpsolverRelation]:
         kwargs = {"schema_relation": schema_relation}
         # Replace with upsolwer query relations list
         #results = self.execute_macro(LIST_RELATIONS_MACRO_NAME, kwargs=kwargs)
-
-        relations = []
-        quote_policy = {"database": True, "schema": True, "identifier": True}
-        _type = "connection"
-        try:
-            _type = self.Relation.get_relation_type(_type)
-        except ValueError:
-            _type = self.Relation.External
-        relations= [self.Relation.create(
-                database="default_glue_catalog",
-                schema="database_16e61b",
-                identifier="table_name",
-                quote_policy=quote_policy,
-                type=_type
-            )]
-        return relations
-
+        # Temporary!!!
+        return []
 
  # may require more build out to make more user friendly to confer with team and community.
---
+++ dbt/adapters/upsolver/relation.py
@@ -0,0 +1,35 @@
+
+from dataclasses import dataclass
+from typing import Optional, Type
+
+#from dbt.adapters.postgres import PostgresRelation
+from dbt.adapters.base import BaseRelation
+from dbt.dataclass_schema import StrEnum
+from dbt.utils import classproperty
+
+
+class UpsolverRelationType(StrEnum):
+    # Built-in materialization types.
+    Table = "table"
+    View = "view"
+    CTE = "cte"
+    External = "external"
+
+    # Materialize-specific materialization types.
+    Connection = "connection"
+    MaterializedView = "materializedview"
+    Job = "job"
+
+
+@dataclass(frozen=True, eq=False, repr=False)
+class UpsolverRelation(BaseRelation):
+    type: Optional[UpsolverRelationType] = None
+
+    # Materialize does not have a 63-character limit for relation
+    # names, unlike PostgreSQL (see dbt-core #2727)
+    def relation_max_name_length(self):
+        return 16384
+
+    @classproperty
+    def get_relation_type(cls) -> Type[UpsolverRelationType]:
+        return UpsolverRelationType
