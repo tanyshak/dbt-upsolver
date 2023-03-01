085f72940e72b632f4e63f6f1e535feed82c7d5b
Tanya Shemet <tanyshak@gmail.com>
2023-03-01 16:13:17 +0100

    new connection syntax with connection_options dict

--- dbt/include/upsolver/macros/materializations/connection.sql
+++ dbt/include/upsolver/macros/materializations/connection.sql
@@ -1,6 +1,7 @@
 {% materialization connection, adapter='upsolver' %}
   {%- set identifier = model['alias'] -%}
   {% set connection_type = config.require('connection_type') %}
+  {% set connection_options = config.require('connection_options') %}
   {%- set curr_datetime = adapter.alter_datetime() -%}
 
   {%- set old_relation = adapter.get_relation(identifier=identifier,
@@ -11,18 +12,23 @@
                                                 database=database,
                                                 type="connection") -%}
 
+
   {{ run_hooks(pre_hooks, inside_transaction=False) }}
   {{ run_hooks(pre_hooks, inside_transaction=True) }}
 
   {% if old_relation %}
     {% call statement('main') -%}
       ALTER {{ connection_type }} CONNECTION {{target_relation.identifier}}
-        SET COMMENT = '{{ curr_datetime }}';
+        {% for k, v in connection_options.items() %}
+          SET {{k}} = {{v}}
+        {% endfor %}
     {%- endcall %}
   {% else %}
     {% call statement('main') -%}
       CREATE {{ connection_type }} CONNECTION {{target_relation.identifier}}
-      {{ sql }}
+      {% for k, v in connection_options.items() %}
+        {{k}} = {{v}}
+      {% endfor %}
     {%- endcall %}
   {% endif %}
 
